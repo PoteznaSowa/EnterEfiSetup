a04f3918355e348dcc202a822be255ead7265985
MightyFixie <maximusf625@gmail.com>
2025-02-08 03:57:24 +0200

    Added a Linux stub

--- EnterEfiSetup.c
+++ EnterEfiSetup.c
@@ -1,24 +1,35 @@
+#ifdef _WIN32
+
+#pragma comment(lib, "NTDLL.lib")
 #include <phnt_windows.h>
 #include <evntrace.h>
 #include <phnt.h>
 
-#include <stdio.h>
+#else
 
-#pragma comment(lib, "NTDLL.lib")
+#define EFI_VARIABLE_NON_VOLATILE		0x00000001
+#define EFI_VARIABLE_BOOTSERVICE_ACCESS	0x00000002
+#define EFI_VARIABLE_RUNTIME_ACCESS		0x00000004
+
+#endif
+
+
+#include <stdio.h>
 
 
 /* https://uefi.org/specs/UEFI/2.10/ */
 
-#define EFI_GLOBAL_VARIABLE \
- {0x8BE4DF61,0x93CA,0x11D2,\
+#define EFI_GLOBAL_VARIABLE	\
+ {0x8BE4DF61,0x93CA,0x11D2,	\
    {0xAA,0x0D,0x00,0xE0,0x98,0x03,0x2B,0x8C}}
-#define EFI_OS_INDICATIONS_BOOT_TO_FW_UI 0x0000000000000001
+#define EFI_OS_INDICATIONS_BOOT_TO_FW_UI	0x0000000000000001
 
 
 //#define NtPrintA(h, iosb, s) \
 //NtWriteFile(h, NULL, NULL, NULL, iosb, s, (ULONG)strlen(s), 0, NULL)
 
 int main() {
+#ifdef _WIN32
 	GUID globalvar = EFI_GLOBAL_VARIABLE;
 	UNICODE_STRING varname1 = RTL_CONSTANT_STRING(L"OsIndications");
 	UNICODE_STRING varname2 = RTL_CONSTANT_STRING(L"OsIndicationsSupported");
@@ -30,6 +41,10 @@ int main() {
 	BOOLEAN r;
 	ULONG attr;
 	IO_STATUS_BLOCK iosb;
+
+	LARGE_INTEGER delay = {
+		.QuadPart = -20'000'000
+	};
 
 	status = RtlAdjustPrivilege(SE_SYSTEM_ENVIRONMENT_PRIVILEGE, TRUE, FALSE, &r);
 	if (status) {
@@ -68,6 +83,9 @@ int main() {
 	puts("The system will enter EFI setup on next boot.");
 
 exit:
-	Sleep(2000);
+	NtDelayExecution(FALSE, &delay);
 	return 0;
+#else	; _WIN32
+	// TODO: Implement for Linux
+#endif
 }
